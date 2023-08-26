b528a2b5fcf3941e5666779cc4ff7aea418b34f8
khalid <khalidalazouni13@gmail.com>
2023-08-26 20:37:08 +0300

    lap (toggle led ) -- creating a static library -- platform.h file

---
+++ unit 3/creating_static_library/can.h
@@ -0,0 +1,4 @@
+#ifndef CAN_H_
+#define CAN_H_
+extern void can_init();
+#endif
---
+++ unit 3/lap_toggle_led/lab1_toggle_led/Debug/Src/main.d
@@ -0,0 +1,5 @@
+Src/main.o: ../Src/main.c ../Src/Register.h ../Src/Platform_Types.h
+
+../Src/Register.h:
+
+../Src/Platform_Types.h:
